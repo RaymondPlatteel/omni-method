366aa5113ea6d17aca3d0dff61a13dd4589563cc
Paul Martel <shmorty@gmail.com>
2024-09-13 22:06:44 -0400

    put menu on home page behind tabs

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {Router} from '@angular/router';
 import {AnnouncementsService} from '../../services/announcements/announcements.service';
 import {UserService} from '../../services/user/user.service';
+import {AlertController, ModalController} from '@ionic/angular';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +13,9 @@ import {UserService} from '../../services/user/user.service';
 export class HomePage {
 
   constructor(
+    private modalCtrl: ModalController,
+    private authService: AuthService,
+    private alertController: AlertController,
     public userService: UserService,
     public announcementService: AnnouncementsService,
     private router: Router
@@ -24,6 +29,37 @@ export class HomePage {
     //   console.log("home page newUser");
     //   alert("Welcome consider adding profile picture");
     // }
+  }
+
+  async logout() {
+    const confirmLogoutButtons = [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        htmlAttributes: {
+          'aria-label': 'cancel',
+        }
+      },
+      {
+        text: 'Log Out',
+        cssClass: 'logout-button-confirm',
+        role: 'confirm',
+        handler: () => {
+          this.modalCtrl.dismiss(null, 'logout');
+          this.authService.logout();
+        },
+        htmlAttributes: {
+          'aria-label': 'logout',
+        }
+      }
+    ];
+    // present logout confirmation
+    const alert = await this.alertController.create({
+      header: 'Log out of your account?',
+      id: 'logout',
+      buttons: confirmLogoutButtons,
+    });
+    await alert.present();
   }
 
 }
